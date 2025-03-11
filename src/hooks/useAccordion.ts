cf413f7a94a3438342e4b0534d7244bd8bc55cb5
MrCorvoDev <mr.corvo.dev@gmail.com>
2025-03-11 16:17:54 +0400

    chore: Remove unused files

--- src/hooks/useAccordion.ts
+++
@@ -1,7 +0,0 @@
-import {useContext} from 'react';
-
-import {AccordionContext} from '../contexts/AccordionContext';
-
-const useAccordion = () => useContext(AccordionContext);
-
-export default useAccordion;
